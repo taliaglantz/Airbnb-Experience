6f2246d6b98f0fe5d14be0d7a18a9fc97fbc0b38
Talia Glantz <taliaglantz13@gmail.com>
2021-11-10 10:45:53 +0000

    Talia - adding experiences and user

--- back-end/db/data/experiences.js
+++ back-end/db/data/experiences.js
@@ -1,20 +1,20 @@
 export default [
   {
-    name: "Yoga with Goats",
-    location: "London",
+    name: 'Yoga with Goats',
+    location: 'London',
     date: [12, 28],
     duration: 90,
-    description: "Take a leisurely walk along a nature path (and meet our friendly critters) to our outdoor yoga studio. Stretch, relax, and center yourself as you prepare to practice yoga while our friendly goats wander around - or even on - you! Soak in the joy of simplicity and happiness that goats can bring! At the end of class, sit peacefully and snuggle one of our adorable baby goats! Other things to note. Please plan to arrive a little early; gates open 15 minutes before class. Please dress for the farm setting and the weather. Campfire will be provided in the colder weather.",
-    category: "Nature and outdoors",
-    image: ["https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/b0c5e4ef-c596-410f-b5d0-3ed4e1acaa52_ya4goc.jpg",
-      "https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/5b78fd86-b19f-4201-a184-d6c65a3d7e88_jlyagr.jpg",
-      "https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/facfdb41-fdfb-4495-8524-74413541f06f_gmccdr.jpg",
-      "https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/07329ec4-421b-4f0c-b1c6-6aab48459870_lvdwlj.jpg",
-      "https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/c714dbf0-c6c6-4c1c-8fad-77ecf9127247_p4asga.jpg",
-      "https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/77cfebbf-6d45-4a6b-a76f-dcef72dda0d2_a0e5tk.jpg"
+    description: 'Take a leisurely walk along a nature path (and meet our friendly critters) to our outdoor yoga studio. Stretch, relax, and center yourself as you prepare to practice yoga while our friendly goats wander around - or even on - you! Soak in the joy of simplicity and happiness that goats can bring! At the end of class, sit peacefully and snuggle one of our adorable baby goats! Other things to note. Please plan to arrive a little early; gates open 15 minutes before class. Please dress for the farm setting and the weather. Campfire will be provided in the colder weather.',
+    category: 'Nature and outdoors',
+    image: ['https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/b0c5e4ef-c596-410f-b5d0-3ed4e1acaa52_ya4goc.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/5b78fd86-b19f-4201-a184-d6c65a3d7e88_jlyagr.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/facfdb41-fdfb-4495-8524-74413541f06f_gmccdr.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/07329ec4-421b-4f0c-b1c6-6aab48459870_lvdwlj.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/c714dbf0-c6c6-4c1c-8fad-77ecf9127247_p4asga.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/77cfebbf-6d45-4a6b-a76f-dcef72dda0d2_a0e5tk.jpg'
     ],
     attendees: [],
-    price: "£28",
+    price: '£28',
     thingsToKnow: [
       {
       header: 'Guest requirements',
@@ -39,10 +39,126 @@ export default [
       ]
     }
   ],
-languages: "English",
-whatIsIncluded: ["tickets"],
+languages: 'English',
+whatIsIncluded: ['tickets'],
 reviews: []//[reviewSchema]
-}
+}, 
+  {
+    name: 'Secret Old London',
+    location: 'London',
+    date: [12, 1],
+    duration: 90,
+    description: 'Walking through the ancient streets of London, you'll learn what lies beneath your feet, from 14th century Black Death victims to a long lost Roman Ampitheatre. Standing by some of the most foreboding locations in the city, I'll tell you about the gruesome executions which took place here, and the partying and revelry which accompanied them! You'll be taken within the shell of a church destroyed in the Blitz, and be led into the park which displays one of the most touching memorials to self sacrifice you could possibly imagine. The walk lasts 1.5 hours and will leave a lasting impression of a different side to London's deep and extraordinary history. Other things to note: Your guide (Matt) will be easily identifiable with a flag and friendly smile. There are no toilet or refreshment breaks during the tour, so please be prepared. Occasionally my friend Jess will guide.',
+    category: 'Art and culture',
+    image: ['https://res.cloudinary.com/dwvbpmbul/image/upload/v1636478693/267de6dd-a905-407a-bac9-40e3642c6a7c_sm3bm0.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636478702/1c03bbdf-84eb-4a9b-b62a-013fbdba0a3d_gstsct.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636478710/c529fa9d-fa17-41e9-8207-c4c0883b8985_g1jdrp.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636478716/77c862e3-e28e-4081-9506-b4c854d7b2c8_s5bozo.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636478722/b00ba4b2-d9e1-46ca-a3dd-336fc19654f8_zduf9e.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636478735/5e68df85-5f4b-491d-8d71-eef63d70d4ab_dxldsr.jpg', 
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636478744/69bc9f87-9dc9-4a52-bbdf-6120f13463ad_njgyyo.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636478755/156899d2-8f7c-4dc5-b7a0-9df35c95cfe7_z0clrr.jpg'
+    ],
+    attendees: [],
+    price: '£15',
+    thingsToKnow: [
+      {
+        header: 'Cancellation policy',
+        text: [
+          'Any experience can be cancelled and fully refunded within 24 hours of purchase, or at least 7 days before the experience starts.'
+        ]
+      },
+      {
+        header: 'Government ID',
+        text: [
+          'You’ll need to take a picture of yourself that matches the photo on your ID. This is so Airbnb can confirm who’s actually going on the experience. You’ll only have to do this once.'
+        ]
+      },
+      {
+      header: 'Guest requirements',
+      text: [
+        'Up to 10 guests ages 18 and up can attend.'
+    }  
+  ],
+languages: 'English',
+whatIsIncluded: ['tickets'],
+reviews: []//[reviewSchema]
+}, 
+  {
+    name: 'Daily evening Art class and wine glass',
+    location: 'London',
+    date: [12, 10],
+    duration: 120,
+    description: 'Add a dose of fun and education to your day in the perfect creative environment of the artist studio with tuition from international professional artist Katerina Kovalova with our daily evening art classes at Wimbledon Art Studios. An exciting way to spend an evening learning something new  getting in touch with your creative self ,meeting like minded people and getting to know more about London creative community at Wimbledon Art Studios complex. We will provide wine and light snacks throughout the lesson. Katerina is a specialist in oil painting and her goal is to help you develop your confidence and achievable levels of creativity with like-minded people who love to paint. Students will explore their personal style at their own pace, making and correcting mistakes as they develop. Katerina’s broad experience enables her to adapt her teaching style to your individual needs and painting style. You will paint either a Studio Still Life or copy one of the many images we can supply or you supply yourself. You may complete a painting in one class or be working on one over several classes or at home. The workshop will be a focused yet very exciting adventure for you to explore oils, charcoals, pastels, watercolours gimmick free with no false promises. We will provide light snacks and refreshments throughout our classes. All materials provided',
+    category: 'Art and culture',
+    image: [
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636479547/6db15f66-aab1-47f9-bf12-3e27c9db5da3_zl12nv.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636479559/a787a75d-b1cb-466b-a262-a8abc0e776ba_wlmyjv.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636479575/1e4e85fd-ac97-45d2-9637-3dc26fe0203d_eapwuh.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636479589/ee3137c1-b98c-43bd-a1b0-bb2d72324ef3_ceav1p.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636479610/329a2518-43ec-4993-a0f1-a2aaff31cc62_oavh9y.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636479634/70137ee7-e1cb-4328-acaf-9bba050ef8e7_iefdam.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636479652/32292bd4-8565-46cc-983e-af6b8574b7c3_cuxgwt.jpg'
+    ],
+    attendees: [],
+    price: '£30',
+    thingsToKnow: [
+      {
+        header: 'Cancellation policy',
+        text: [
+          'Any experience can be cancelled and fully refunded within 24 hours of purchase, or at least 7 days before the experience starts.'
+        ]
+      },
+      {
+        header: 'Guest requirements',
+        text: [
+          'Up to 6 guests ages 10 and up can attend. Parents may also bring children under 2 years of age.'
+      }
+  ],
+languages: 'English',
+whatIsIncluded: ['tickets', 'materials', 'snacks', 'wine'],
+reviews: []//[reviewSchema]
+}, 
+  {
+    name: 'East London Street Art Tour',
+    location: 'London',
+    date: [12, 7],
+    duration: 120,
+    description: 'We\'ll explore the street and surfaces of the vibrant East London through Graffiti and Street Art. We\'ll start from Shoreditch Overground station and we\'ll end in Hanbury Street, not before viewing a cople of pieces by Banksy. We\'ll discover new and old pieces, big and tiny artistic interventions, find out about the artists, learn about techniques and have an insight into the artistic scene. Be prepared to walk in any weather. Bring comfortable shoes and a raincoat or an umbrella and don\'t forget your camera! Great experience for groups and fun for kids! Other things to note: Bring your camera, an umbrella or a raincoat and bottle of water. Make sure to wear warm clothes and comfortable shoes.',
+    category: 'Art and culture',
+    image: [
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636480044/9b2b98af-5c6c-430e-a301-f9ba94be27ac_hghltr.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636480064/363f7c76-a621-47e9-9709-2e6f94ddd883_y8fxx5.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636480085/380fd86b-7328-4b6e-86bc-f50c6d9b3bb7_lxnnub.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636480108/edd9a8f1-f15d-4055-80d9-e30c3a56ea19_qbjfnf.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636480133/b9d6d318-5025-44cd-b5ec-0434a31fc2a8_w3wspz.jpg',
+      'https://res.cloudinary.com/dwvbpmbul/image/upload/v1636480157/12f371c2-5886-4137-b92c-65ac8dff9a06_a1c8e6.jpg'
+    ],
+    attendees: [],
+    price: '£16',
+    thingsToKnow: [
+      {
+        header: 'Cancellation policy',
+        text: [
+          'Any experience can be cancelled and fully refunded within 24 hours of purchase, or at least 7 days before the experience starts.'
+        ]
+      },
+      {
+        header: 'Government ID',
+        text: [
+          'You’ll need to take a picture of yourself that matches the photo on your ID. This is so Airbnb can confirm who’s actually going on the experience. You’ll only have to do this once.'
+        ]
+      },
+      {
+        header: 'Guest requirements',
+        text: [
+          'Up to 10 guests of all ages can attend.'
+      }
+  ],
+languages: 'English',
+whatIsIncluded: ['tickets'],
+reviews: []//[reviewSchema]
+}, 
 ]
 
 // Jamie: London - entertainment
---
+++ back-end/db/data/user.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    username: "taliaglantz",
+    firstName: "Talia",
+    lastName: "Glantz",
+    email: "talia@email.com",
+    mobile: 07893448506,
+    profilePicture: "https://res.cloudinary.com/dwvbpmbul/image/upload/v1636477436/images_klpw0s.jpg",
+    password: "pass",
+    isHost: false,
+    location: "London",
+    languages: ["English"],
+    occupation: "Student",
+    about: "Hello, I'm Talia and I love exploring London 😊",
+    dob: "",
+    wishlist: [], 
+    experiences: []
+  }
+]
