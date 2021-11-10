da94ff31f58acd07feb9d4a0e9d7992150abbd77
rhonabpetersen <rhonabpetersen@gmail.com>
2021-11-10 14:20:51 +0100

    pushing back-end

--- back-end/config/environment.js
+++ back-end/config/environment.js
@@ -1,4 +1,4 @@
 export const port = 4000
 // export const dbURI = 'mongodb://127.0.0.1:27017/api-mongodb-start'
-export const dbURI = 'mongodb://localhost/SEI-Project-3-59'
+export const dbURI = 'mongodb://localhost/back-end'
 export const secret = "this is not airbnb"
