e2fd46d25229f0c3d7bd8b1116914fdbf1524ec2
Anna Wilczynska <anna.wilcz@outlook.com>
2021-11-10 10:19:15 +0000

    Anna - updating collections

--- back-end/db/data/experiences.js
+++ back-end/db/data/experiences.js
@@ -1,51 +1,181 @@
 export default [
   {
-    name: "Yoga with Goats",
-    location: "London",
+    name: 'Yoga with Goats',
+    location: 'London',
     date: [12, 28],
     duration: 90,
-    description: "Take a leisurely walk along a nature path (and meet our friendly critters) to our outdoor yoga studio. Stretch, relax, and center yourself as you prepare to practice yoga while our friendly goats wander around - or even on - you! Soak in the joy of simplicity and happiness that goats can bring! At the end of class, sit peacefully and snuggle one of our adorable baby goats! Other things to note. Please plan to arrive a little early; gates open 15 minutes before class. Please dress for the farm setting and the weather. Campfire will be provided in the colder weather.",
-    category: "Nature and outdoors",
-    image: ["https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/b0c5e4ef-c596-410f-b5d0-3ed4e1acaa52_ya4goc.jpg",
-      "https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/5b78fd86-b19f-4201-a184-d6c65a3d7e88_jlyagr.jpg",
-      "https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/facfdb41-fdfb-4495-8524-74413541f06f_gmccdr.jpg",
-      "https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/07329ec4-421b-4f0c-b1c6-6aab48459870_lvdwlj.jpg",
-      "https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/c714dbf0-c6c6-4c1c-8fad-77ecf9127247_p4asga.jpg",
-      "https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/77cfebbf-6d45-4a6b-a76f-dcef72dda0d2_a0e5tk.jpg"
+    description: 'Take a leisurely walk along a nature path (and meet our friendly critters) to our outdoor yoga studio. Stretch, relax, and center yourself as you prepare to practice yoga while our friendly goats wander around - or even on - you! Soak in the joy of simplicity and happiness that goats can bring! At the end of class, sit peacefully and snuggle one of our adorable baby goats! Other things to note. Please plan to arrive a little early; gates open 15 minutes before class. Please dress for the farm setting and the weather. Campfire will be provided in the colder weather.',
+    category: 'Nature and outdoors',
+    image: [
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/b0c5e4ef-c596-410f-b5d0-3ed4e1acaa52_ya4goc.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/5b78fd86-b19f-4201-a184-d6c65a3d7e88_jlyagr.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/facfdb41-fdfb-4495-8524-74413541f06f_gmccdr.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/07329ec4-421b-4f0c-b1c6-6aab48459870_lvdwlj.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/c714dbf0-c6c6-4c1c-8fad-77ecf9127247_p4asga.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636473288/Experience-Images/77cfebbf-6d45-4a6b-a76f-dcef72dda0d2_a0e5tk.jpg'
     ],
     attendees: [],
-    price: "£28",
+    price: '£28',
     thingsToKnow: [
       {
-      header: 'Guest requirements',
-      text: [
-        'You will need a photo that matches the one on your ID to confirm who is actually going on this experience.',
-        'Guests ages 6 and up can attend, up to 10 guests in total.',
-        'All participants must wear a protective face covering and practise social distancing.'
-      ]
-    },
-    {
-      header: 'What to bring',
-      text: [
-        'Yoga mat',
-        'Water bottle',
-        'Love of goats'
-      ]
-    },
-    {
-      header: 'Cancellation policy',
-      text: [
-        'Cancel within 24 hours of purchasing or at least 7 days before the experience'
-      ]
-    }
-  ],
-languages: "English",
-whatIsIncluded: ["tickets"],
-reviews: []//[reviewSchema]
-}
-]
+        header: 'Guest requirements',
+        text: [
+          'You will need a photo that matches the one on your ID to confirm who is actually going on this experience.',
+          'Guests ages 6 and up can attend, up to 10 guests in total.',
+          'All participants must wear a protective face covering and practise social distancing.'
+        ]
+      },
+      {
+        header: 'What to bring',
+        text: [
+          'Yoga mat',
+          'Water bottle',
+          'Love of goats'
+        ]
+      },
+      {
+        header: 'Cancellation policy',
+        text: [
+          'Cancel within 24 hours of purchasing or at least 7 days before the experience'
+        ]
+      }
+    ],
+    languages: 'English',
+    whatIsIncluded: ['tickets'],
+    reviews: []
+  },
 
-// Jamie: London - entertainment
-// Rhona: London - nature and outdoors
-// Anna: London - food and drink
-// Talia: London - arts and culture
+  {
+    name: 'Eat 8 British foods & See 30+ sights!',
+    location: 'London',
+    date: [4, 6, 12, 21],
+    duration: 300,
+    description: 'We will explore 30+ of London’s major landmarks & the best of British foods. This will give you a good idea of the history & tastes of Europe’s best-loved city After the walking tour we will stop for an hour & taste the best of British foods in one of London\'s oldest food markets : Borough market. Its a bustling hub of smells and flavours Foods you will try include : Fish & Chips (mushy peas/gravy), Ale, Pie & Mash (gravy) and a world famous dessert.. At the start we will head through Green Park to Buckingham Palace, where we\'ll watch the world-famous Changing of the Guard ceremony (if it is on). We then head to Westminster. Sights we\'ll see along the way include Parliament Square, St James Palace, Westminster Abbey, Big Ben (having a face lift at the moment), & the Houses of Parliament We then go to the London Bridge area, where we will see Shakespeare’s Globe, the Shard, HMS Belfast, Tower Bridge & the Tower of London. Plus many other sights! You will be hungry and thirsty (hopefully) at this point : we then go for a feast of the best of British foods Other things to note The food tasting is at the end of the 5 hour walking tour.',
+    category: 'Food and drink',
+    image: [
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636478802/Experience-Images/a3f10a82-ab96-49c1-aaf5-3380ebbdb1a8_xmslav.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636478802/Experience-Images/f63bd5ab-0d1e-4907-afa0-9258463b1da4_uilksa.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636478802/Experience-Images/fab78b91-d2bc-4497-8298-8dac4c22583e_toif2x.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636478801/Experience-Images/79322a49-008e-4a3a-a9d6-7636ff2bbf82_lycozc.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636478801/Experience-Images/bc7f47f6-0a48-4f60-91ae-0c0e12b680a3_xghzpr.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636478801/Experience-Images/b4ed66f8-0b82-4a84-93a9-4bd85bd02f32_sx7g6f.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636478801/Experience-Images/6da304d8-5b1d-4082-91c4-541edc6f2614_s6o4t1.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636478801/Experience-Images/326934ed-a67c-4803-8447-6b012dcd9bcb_wxmxqq.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636478801/Experience-Images/315dfe3e-44e6-4abd-a527-e6340c93e02f_wt4pm4.jpg'
+    ],
+    attendees: [],
+    price: '£72',
+    thingsToKnow: [
+      {
+        header: 'What to bring',
+        text: [
+          'Good camera',
+          'Topped up Oyster card',
+          'Umbrella'
+        ]
+      },
+      {
+        header: 'Cancellation policy',
+        text: [
+          'Cancel within 24 hours of purchasing or at least 7 days before the experience'
+        ]
+      }
+    ],
+    languages: 'English',
+    whatIsIncluded: ['food', 'drinks'],
+    reviews: []
+  },
+
+  {
+    name: 'Royal High Tea at Kensington Palace',
+    location: 'London',
+    date: [2, 3, 10, 15, 18, 22],
+    duration: 120,
+    description: 'Experience the original Royal English Tea with traditional sandwiches, cakes & scones on the grounds of Kensington Palace. First, we will have a leisurely stroll around Kensington Gardens and learn of the part Hyde Park played in London’s development. We will see the beautiful gardens Prince Albert created for his wife Queen Victoria and take in the spectacular views as we approach the Palace where Prince Harry and William grew up. The best setting to discover the new Diana\'s bronze statue. We will then experience the full splendor of High Tea in a breathtaking landmark and give you personal insights into our royal family and the fascinating history of our national drink and the ceremony of afternoon tea. Gratuities are entirely discretionary. The High Tea does not include any alcohol but a drinks menu is available when you visit for a supplementary cost. Please contact for Private, Corporate & Large Group Bookings or if dates are not available. Great for groups, group activity, group tour. Other things to note This High Tea is not including any alcohol, Any upgrade is available Please speak to your guide prior to the tour about your allergies or food intolerances or send us a message Please be advised that the food on the tour is not prepared by us and The Caterer offers Vegan and Gluten-Free options',
+    category: 'Food and drink',
+    image: [
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479440/Experience-Images/b3c29e41-d5a4-40ae-9e4b-5f061e6bade4_yllmuk.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479440/Experience-Images/93565d2f-ce93-44dc-9b8c-480ce5d925b9_f1v9yf.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479440/Experience-Images/35da8e98-e107-47e7-9238-7dbb2bfa9adc_kj7mwx.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479440/Experience-Images/eed94081-48e6-42a5-b8af-4557df245add_vvsbfw.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479440/Experience-Images/3b5c703b-a24d-4631-9274-555f6d14dc45_r5xmkz.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479440/Experience-Images/d3eb5857-6abc-4db4-b43b-4daa80034855_m02mi8.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479441/Experience-Images/102c914b-86ce-44f3-9f4a-809b7b23f4c4_cs02qe.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479440/Experience-Images/2f36be41-e994-45f1-8e5a-baa8c771a036_cblt4i.jpg'
+    ],
+    attendees: [],
+    price: '£79',
+    thingsToKnow: [
+      {
+        header: 'Guest requirements',
+        text: [
+          'Guests ages 2 and up can attend, up to 10 guests in total. Parents can also bring children under 2 years of age.',
+          'All participants must wear a protective face covering and practise social distancing.'
+        ]
+      },
+      {
+        header: 'What to bring',
+        text: [
+          'Comfy shoes',
+          'Umbrella',
+          'Warm coat coat, or sunscreen to withstand the beautiful English weather :)',
+          'Camera',
+          'Water'
+        ]
+      },
+      {
+        header: 'Cancellation policy',
+        text: [
+          'Cancel within 24 hours of purchasing or at least 7 days before the experience starts for a full refund.'
+        ]
+      }
+    ],
+    languages: 'English',
+    whatIsIncluded: ['food', 'drinks', 'tickets'],
+    reviews: []
+  }, 
+
+  {
+    name: 'Baking Brownies at Melt',
+    location: 'London',
+    date: [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28],
+    duration: 120,
+    description: 'You will learn how to Bake Brownies to perfection with Melt\'s World Class Chocolatiers. Bake your own Brownies or Mississippi Mud Pie with a special Melt recipe. This is a Masterclass in chocolate. Take home your creations in one of our stylish bags and boxes. The class will be held at Melt\'s Kitchen Luxury Boutique in the heart of Historic Notting Hill. One of the world\'s greatest urban environments and a very fashionable shopping area.',
+    category: 'Food and drink',
+    image: [
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479914/Experience-Images/2b86d503-8843-402f-8636-6d75fc707548_qxl2pu.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479914/Experience-Images/a0b282e1-9f02-427c-ae4e-5f287a86f2ee_kezthr.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479914/Experience-Images/554cc791-265b-4ff7-b20d-5ff6142b8b16_qljlbz.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479914/Experience-Images/48599979-0e4f-4e2e-b182-34b2adf39c78_fnk8q7.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479914/Experience-Images/db150b57-d5f8-48fd-9e6b-b5df17699653_g4nnio.jpg',
+      'https://res.cloudinary.com/dulbdr0in/image/upload/v1636479914/Experience-Images/e0e0d717-46e6-4cbf-b6d9-48fafd5d488f_mvve4w.jpg'
+    ],
+    attendees: [],
+    price: '£35',
+    thingsToKnow: [
+      {
+        header: 'Guest requirements',
+        text: [
+          'Guests ages 12 and up can attend, up to 8 guests in total.',
+          'All participants must wear a protective face covering and practise social distancing.',
+          'Let the Host know of any allergies or dietary restrictions.'
+        ]
+      },
+      {
+        header: 'What to bring',
+        text: [
+          'Enthusiasm and a willingness to learn from the professionals'
+        ]
+      },
+      {
+        header: 'Cancellation policy',
+        text: [
+          'Cancel within 24 hours of purchasing or at least 7 days before the experience starts for a full refund.'
+        ]
+      }
+    ],
+    languages: 'English',
+    whatIsIncluded: ['food', 'drinks', 'EQUIPMENT'],
+    reviews: []
+  }
+]
---
+++ back-end/db/data/users.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    username: 'Anna',
+    firstName: 'Anna',
+    lastName: 'Wilczynska',
+    email: 'anna@email.com',
+    mobile: '01234567890',
+    profilePicture: 'https://res.cloudinary.com/dulbdr0in/image/upload/v1636476103/Experience-Images/download_ad3kmc.jpg',
+    password: 'pass',
+    isHost: true,
+    location: 'London',
+    languages: ['English', 'Polish'],
+    occupation: 'Software engineer',
+    about: 'Hi, this is Anna! Nice to meet you 😀!',
+    dob: '1995-06-24',
+    wishlist: [], 
+    experiences: []
+  }
+]
