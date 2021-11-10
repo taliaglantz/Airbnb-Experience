ef662c49073241734cc39cf778e40d9f99cdea84
Jamie Joahill <jamie.joahill@gmail.com>
2021-11-10 10:14:40 +0000

    pJamie Pushing experinces and users

---
+++ back-end/db/data/users.js
@@ -0,0 +1,73 @@
+export default [
+  {
+    username: 'Anna',
+    firstName: 'Anna',
+    lastName: 'Wilczynska',
+    email: 'anna@email.com',
+    mobile: '01234567890',
+    profilePicture: 'https://res.cloudinary.com/dulbdr0in/image/upload/v1636476103/Experience-Images/download_ad3kmc.jpg',
+    password: 'pass',
+    isHost: false,
+    location: 'London',
+    languages: ['English', 'Polish'],
+    occupation: '',
+    about: 'Hi, this is Anna! Nice to meet you 😀!',
+    dob: '',
+    wishlist: [], 
+    experiences: []
+  },
+  {
+    username: 'Jamie',
+    firstName: 'Jamie',
+    lastName: 'Joahill',
+    email: 'jamie@email.com',
+    mobile: '07999999999',
+    profilePicture: 'https://res.cloudinary.com/dmpvulj3q/image/upload/v1636473179/samples/people/jazz.jpg',
+    password: 'pass',
+    isHost: false,
+    location: 'London',
+    languages: ['English'],
+    occupation: 'Developer',
+    about: 'Hi, this is Jamie! Whats up...',
+    dob: new Date(1993, 7, 29),
+    wishlist: [], 
+    experiences: []
+  },
+  {
+    username: 'JohnSmith',
+    firstName: 'John',
+    lastName: 'Smith',
+    email: 'johnsmith@email.com',
+    mobile: '07888888888',
+    profilePicture: 'https://res.cloudinary.com/dmpvulj3q/image/upload/v1636473179/samples/people/jazz.jpg',
+    password: 'pass',
+    isHost: false,
+    location: 'London',
+    languages: ['English'],
+    occupation: 'Bartender',
+    about: 'Hi, this is John! How are things..',
+    dob: new Date(1991, 2, 12),
+    wishlist: [], 
+    experiences: []
+  },
+  {
+    username: 'SteveAllen',
+    firstName: 'Steve',
+    lastName: 'Allen',
+    email: 'steveallen@email.com',
+    mobile: '07666666666',
+    profilePicture: 'https://res.cloudinary.com/dmpvulj3q/image/upload/v1636473179/samples/people/jazz.jpg',
+    password: 'pass',
+    isHost: false,
+    location: 'London',
+    languages: ['English'],
+    occupation: 'Lawyer',
+    about: 'Hi, this is John! How are things..',
+    dob: new Date(1991, 2, 12),
+    wishlist: [], 
+    experiences: []
+  }
+]
+
+// console.log('Will display date as -> ',event.toDateString()) e.g. "Sun Aug 29 1993"
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toDateString
