506911aa50fc0057e6154c0b844a476ebcaa587f
rhonabpetersen <rhonabpetersen@gmail.com>
2021-11-10 14:09:51 +0100

    push backend

--- back-end/config/router.js
+++ back-end/config/router.js
@@ -1,2 +1,32 @@
+import express from 'express'
+import { addAComment, deleteAComment, getAllExperiences, addExperience, getSingleExperience, deleteExperince, updatedExperience } from '../controllers/experiences.js'
+import { getUserProfile } from '../controllers/users.js'
 
+const router = express.Router()
+
+router.route('/experiences')
+  .get(getAllExperiences)
+  .post( addExperience )
+
+router.route('experiences/:id')
+  .get(getSingleExperience)
+  .put(updatedExperience)
+  .delete(deleteExperince)
+
+router.route('/experiences/:id/comments')
+  .post(addComment)
+
+router.route('/experiences/:id/comments/:commentId')
+  .delete(deleteAComment)
+
+router.route('/register')
+  .post(registerUser)
+
+router.route('/login')
+  .post(loginUser)
+
+router.route('profile')
+  .get(getUserProfile)
+
+export default router
 
