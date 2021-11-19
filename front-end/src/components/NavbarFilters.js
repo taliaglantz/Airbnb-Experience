78646606d7b3d3b092ac1407d3dbf3dc6cc018fd
rhonabpetersen <rhonabpetersen@gmail.com>
2021-11-19 14:19:23 +0100

    auth

--- front-end/src/components/NavbarFilters.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NavbarFilters = () => {
-
-  return (
-    <p>Navbar Filters</p>
-  )
-}
-export default NavbarFilters
---
+++ front-end/src/components/NewUser.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const NewUser = ({ newUser, createNewUser, setVisibilityLogin }) => {
+  const handleClick = (text) => {
+    createNewUser(false)
+    if (text === 'Log in and explore!') {
+      setVisibilityLogin(true)
+    }
+  }
+
+  return (
+    <div className='popup-screen'>
+      <div className='popup-window'>
+        <i className='close icon' onClick={(event) => handleClick(event.target.innerText)} />
+        <div className='popup-header'>
+          <h5>Welcome <span className='coral'>{newUser}</span>!</h5>
+          <hr />
+        </div>
+        <div className='popup-form-container'>
+          <h4 className=''>🤍&nbsp;🤍&nbsp;🤍&nbsp;&nbsp;</h4>
+          <h4 className=''>Your account has been successfully created.</h4>
+          <button onClick={(event) => handleClick(event.target.innerText)} className='coral-button' type='submit'>Log in and explore!</button>
+        </div>
+      </div>
+    </div>
+  )
+
+}
+export default NewUser
